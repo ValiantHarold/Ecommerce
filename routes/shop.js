9c134cf6d22d93831c2f3ab58327dae60a0dda78
ValiantWolf <25witt25@gmail.com>
2021-02-13 14:01:13 -0700

    stripe makes me frown :(

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const express = require('express');
+const express = require('express'); 
 
 const shopController = require('../controllers/shop');
 const isAuth = require('../middleware/is-auth');
@@ -19,11 +19,14 @@ router.post('/cart', isAuth, shopController.postCart);
 
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.get('/checkout', isAuth, shopController.getCheckout);
+router.post('/create-order', isAuth, shopController.postOrder);
 
-router.get('/checkout/success', shopController.getCheckoutSuccess);
+//stripe makes me frown :(
+// router.get('/checkout', isAuth, shopController.getCheckout);
 
-router.get('/checkout/cancel', shopController.getCheckout);
+// router.get('/checkout/success', shopController.getCheckoutSuccess);
+
+// router.get('/checkout/cancel', shopController.getCheckout);
 
 router.get('/orders', isAuth, shopController.getOrders);
 
